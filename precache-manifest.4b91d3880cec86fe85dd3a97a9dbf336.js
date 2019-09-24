25aa6fe8bb6df4d508e0dfb448926a3567a73f26
MOSES <MOSESLAZARO2019@outlook.com>
2019-09-24 18:29:05 +0300

    Updates

---
+++ precache-manifest.4b91d3880cec86fe85dd3a97a9dbf336.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d29ca557d55143697df3e391725c3dc",
+    "url": "/Search-component/index.html"
+  },
+  {
+    "revision": "8a7981c6665a1bb9dab6",
+    "url": "/Search-component/static/css/main.a648de79.chunk.css"
+  },
+  {
+    "revision": "5a605abb8bf853139d1e",
+    "url": "/Search-component/static/js/2.bb43ea5a.chunk.js"
+  },
+  {
+    "revision": "8a7981c6665a1bb9dab6",
+    "url": "/Search-component/static/js/main.2d750861.chunk.js"
+  },
+  {
+    "revision": "b3e49f6ab233d99420a6",
+    "url": "/Search-component/static/js/runtime-main.59ec941b.js"
+  }
+]);
